3c8743a16d3d8a369307d980f8d9e2c1057664a1
Ahmad Ramadan <ark@Ahmads-MacBook-Pro.local>
2023-09-15 14:21:03 +0300

    First Commit

---
+++ src/SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    uint256 public storedData;
+
+    struct Person {
+        uint256 myStoredData;
+        string name;
+    }
+
+    Person[] public people;
+
+    mapping(string => uint256) public nameToMyStoredData;
+
+    function set(uint256 x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+
+    function addPerson(uint256 _myStoredData, string memory _name) public {
+        people.push(Person(_myStoredData, _name));
+        nameToMyStoredData[_name] = _myStoredData;
+    }
+}
